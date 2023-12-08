600290e814e563c14b5d7256150f0c4bcc06a474
raining0001 <122534481+raining0001@users.noreply.github.com>
2023-12-08 21:06:24 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,33 +3,38 @@ from value import coins , coins_val
 from drinks import recepies, price
 from stock import report
 
-
 def user_money():
     print("Please insert coins.")
     coins["quarter"] = int(input("How many quarters?: "))
     coins["dime"] = int(input("How many dimes?: "))
     coins["nickel"] = int(input("How many nickels?: "))
     coins["penny"] = int(input("How many pennies?: "))
-  
+
     global user_sum
     user_sum = sum(coins_val[key] * value for key, value in coins.items())
 
 def check_resources(user):
     for key, value in recepies[user].items():
         if value > report["resources"][key]:
-            print(f"Sorry not enough resources")
+            print(f"Sorry not enough {key}.")
             return False
     return True
 
 def drink_change():
-    global machine_sum
+    global machine_sum, user_sum
     change = user_sum - price[user]["money"]
     machine_sum += price[user]["money"]
     print(f"Here is {change} in change.")
-    print("Here is your latte, Enjoy!")
+    print(f"Here is your {user}, Enjoy!")
+
+    for key, value in recepies[user].items():
+        report["resources"][key] -= value
+
+    for key, value in coins.items():
+        report["money"][key] += value
 
 machine_sum = sum(report["money"].values())
-
+user_sum = sum(coins.values())
 
 print(logo)
 print(menu)
@@ -44,7 +49,6 @@ while True:
             drink_change()
         elif user_sum == price[user]["money"]:
             machine_sum += user_sum
-            print("Here is your latte, Enjoy!")
+            print(f"Here is your {user}, Enjoy!")
         else:
             print("Sorry not enough money. Money refunded")
-        break
